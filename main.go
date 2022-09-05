742cf31fe88e7dbaacd7173a6b44b880a69d11b3
Marton Pentek <pentek.marci@gmail.com>
2022-09-05 16:09:34 +0200

    shaping the kube secrets to work

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	acme "github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"os"
 	"simply-dns-webhook/client"
-	"sync"
+)
+
+const (
+	apiUrl = "https://api.simply.com/2"
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
@@ -26,31 +35,73 @@ func main() {
 	)
 }
 
+type customDNSProviderConfig struct {
+	// Change the two fields below according to the format of the configuration
+	// to be decoded.
+	// These fields will be set by users in the
+	// `issuer.spec.acme.dns01.providers.webhook.config` field.
+
+	AccountName string `json:"accountName"`
+	ApiKey      string `json:"api-key"`
+	SecretRef   string `json:"secretName"`
+}
+
 type SimplyDnsSolver struct {
-	Username string `json:"username"` //Simply user/account name
-	Password string `json:"password"` //Simply api key for corresponding account
-	client   client.SimplyClient
-	sync.RWMutex
+	client     client.SimplyClient
+	kubeClient *kubernetes.Clientset
 }
 
 func (e *SimplyDnsSolver) Name() string {
 	return "simply-dns-solver"
 }
-func (e *SimplyDnsSolver) Present(ch *acme.ChallengeRequest) error {
-	e.Lock()
+func (e *SimplyDnsSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+	secretName := cfg.SecretRef
+	sec, err := e.kubeClient.CoreV1().Secrets(ch.ResourceNamespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+	//todo:error handling
+	apiKey, err := stringFromSecretData(&sec.Data, "api-key")
+	//todo:error handling
+	apiKey = apiKey + "" //bullshit code coming here
 	e.client.AddTxtRecord(ch.DNSName, ch.Key)
-	e.Unlock()
 	return nil
 }
 
-func (e *SimplyDnsSolver) CleanUp(ch *acme.ChallengeRequest) error {
-	e.Lock()
+func (e *SimplyDnsSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
 	Id := e.client.GetTxtRecord(ch.DNSName)
 	e.client.RemoveTxtRecord(Id)
-	e.Unlock()
 	return nil
 }
 
 func (e *SimplyDnsSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	cl, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return err
+	}
+
+	e.kubeClient = cl
+
 	return nil
+}
+
+func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
+	cfg := customDNSProviderConfig{}
+
+	if cfgJSON == nil {
+		return cfg, nil
+	}
+	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
+		return cfg, fmt.Errorf("error decoding solver config: %v", err)
+	}
+	return cfg, nil
+}
+
+func stringFromSecretData(secretData *map[string][]byte, key string) (string, error) {
+	data, ok := (*secretData)[key]
+	if !ok {
+		return "", fmt.Errorf("key %q not found in secret data", key)
+	}
+	return string(data), nil
 }
--- main_test.go
+++ main_test.go
@@ -17,18 +17,18 @@ func TestRunsSuite(t *testing.T) {
 	//
 
 	// Uncomment the below fixture when implementing your custom DNS provider
-	//fixture := dns.NewFixture(&customDNSProviderSolver{},
-	//	dns.SetResolvedZone(zone),
-	//	dns.SetAllowAmbientCredentials(false),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
-	//)
 	fixture := dns.NewFixture(&SimplyDnsSolver{},
-		dns.SetResolvedZone("example.com."),
+		dns.SetResolvedZone(zone),
+		dns.SetAllowAmbientCredentials(false),
 		dns.SetManifestPath("testdata/my-custom-solver"),
-		dns.SetDNSServer("127.0.0.1:59351"),
-		dns.SetUseAuthoritative(false),
+		//dns.SetBinariesPath("_test/kubebuilder/bin"),
 	)
+	//fixture := dns.NewFixture(&SimplyDnsSolver{},
+	//	dns.SetResolvedZone("example.com."),
+	//	dns.SetManifestPath("testdata/my-custom-solver"),
+	//	dns.SetDNSServer("127.0.0.1:59351"),
+	//	dns.SetUseAuthoritative(false),
+	//)
 	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
 	//fixture.RunConformance(t)
 	fixture.RunBasic(t)
