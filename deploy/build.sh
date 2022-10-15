3f75d73ab3a42184888576d57303a89c5fbd99e5
Marton Pentek <pentek.marci@gmail.com>
2022-10-15 20:45:23 +0200

    test chart build script

---
+++ deploy/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#This is a script for building and indexing the chart.
+
+helm lint simply-dns-webhook
+helm package simply-dns-webhook
+helm repo index . --url https://runnerm.github.io/simply-dns-webhook/
