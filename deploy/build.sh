1e281fab21d9988ff3d56fb01cf154fdc9fb2e8d
Márton Péntek <pentek.marci@gmail.com>
2023-05-26 10:26:54 +0200

    fix index

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,5 +2,5 @@
 #This is a script for building and indexing the chart.
 
 helm lint simply-dns-webhook
-#helm package simply-dns-webhook
-helm repo index . --url https://runnerm.github.io/simply-dns-webhook/ --merge index.yaml
+helm package simply-dns-webhook
+helm repo index . --url https://runnerm.github.io/simply-dns-webhook/
